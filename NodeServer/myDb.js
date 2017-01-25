7dc38c42efc1e1ff231ef5899f7604a96503d5f7
MattJones <Matt.r.jones1995@gmail.com>
2017-01-25 11:46:16 +0000

    routing and sql starting to be implemented. Need to think how to keep this clean

--- NodeServer/myDb.js
+++ NodeServer/myDb.js
@@ -1,9 +1,48 @@
 (function () {
     var myDb = function () {
-        var fs = require("fs");
-        var file = "test.db";
-        var sqlite3 = require("sqlite3").verbose();
-        var db = new sqlite3.Database(file);
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'fyp123',
+            database: 'fyp_db'
+        });
+
+        var createConnection = function () {
+            connection.connect(function (err) {
+                if (err) {
+                    console.error('error connecting: ' + err.stack);
+                    return;
+                }
+
+                console.log('connected as id ' + connection.threadId);
+            });
+        }
+
+        var createTableIfDoesNotExist = function () {
+            var query = "SELECT 1 FROM testtable LIMIT 1";
+            connection.query(query, function (err) {
+                if (err) {
+                    createTable();
+                }
+            })
+        }
+
+        var createTable = function () {
+            console.log("creating table");
+            var query = 'CREATE TABLE rooms (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name TEXT, max_occupancy INT)';
+            connection.query(query, function (err) {
+                if (err) {
+                    console.error('error connecting: ' + err.stack);
+                    return;
+                }
+
+                console.log("table created");
+            })
+        }
+
+        createConnection();
+        createTableIfDoesNotExist();
     }
 
     module.exports.getDbConnection = function () {
--- NodeServer/server.js
+++ NodeServer/server.js
@@ -1,8 +1,25 @@
+var express = require("express");
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var bodyParser = require('body-parser');
 var myDb = require('./myDb');
-var app = connect().use(serveStatic("web")).listen(8080, function () {
+
+app = express();
+
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+app.use('/fypApi', router);
+
+app.use(serveStatic("web")).listen(8080, function () {
     console.log('Server running on 8080...');
-    console.log(myDb);
-    console.log(myDb.getDbConnection());
 });
