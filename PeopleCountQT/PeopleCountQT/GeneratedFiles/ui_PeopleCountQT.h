63416c91c36f5cd5fc6512dac06b213231af23c5
MattJones <Matt.r.jones1995@gmail.com>
2017-02-26 13:46:01 +0000

    update ui

--- PeopleCountQT/PeopleCountQT/GeneratedFiles/ui_PeopleCountQT.h
+++ PeopleCountQT/PeopleCountQT/GeneratedFiles/ui_PeopleCountQT.h
@@ -17,6 +17,7 @@
 #include <QtWidgets/QFormLayout>
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
@@ -35,44 +36,50 @@ class Ui_PeopleCountQTClass
 public:
     QWidget *centralWidget;
     QGridLayout *gridLayout_2;
-    QLabel *label;
     QScrollArea *scrollArea;
     QWidget *scrollAreaWidgetContents_3;
     QVBoxLayout *verticalLayout_3;
     QGroupBox *groupBox_13;
     QVBoxLayout *verticalLayout_4;
+    QGroupBox *groupBox_19;
+    QFormLayout *formLayout_6;
+    QLabel *label_35;
+    QComboBox *comboBox_3;
     QGroupBox *groupBox_14;
     QFormLayout *formLayout;
     QLabel *label_24;
-    QSpinBox *spinBox_19;
+    QSpinBox *minArea;
     QGroupBox *groupBox_15;
     QFormLayout *formLayout_2;
     QLabel *label_25;
     QLabel *label_26;
     QLabel *label_27;
-    QSpinBox *spinBox_20;
-    QSpinBox *spinBox_21;
-    QSpinBox *spinBox_22;
+    QSpinBox *closeKernelSize;
+    QSpinBox *openKernelSize;
+    QSpinBox *thresholdValue;
     QGroupBox *groupBox_16;
     QFormLayout *formLayout_3;
     QLabel *label_28;
     QLabel *label_29;
     QLabel *label_30;
-    QComboBox *comboBox_3;
-    QSpinBox *spinBox_23;
-    QSpinBox *spinBox_24;
+    QComboBox *orientation;
+    QSpinBox *inLinePercentage;
+    QSpinBox *outLinePercentage;
     QGroupBox *groupBox_17;
     QFormLayout *formLayout_4;
     QLabel *label_31;
     QLabel *label_32;
-    QSpinBox *spinBox_25;
-    QSpinBox *spinBox_26;
+    QSpinBox *maxWidth;
+    QSpinBox *maxHeight;
     QGroupBox *groupBox_18;
     QFormLayout *formLayout_5;
     QLabel *label_33;
     QLabel *label_34;
-    QLineEdit *lineEdit_3;
-    QSpinBox *spinBox_27;
+    QLineEdit *ipAddress;
+    QSpinBox *portNumber;
+    QLabel *label;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton;
     QPushButton *loadVideo;
     QMenuBar *menuBar;
 
@@ -96,31 +103,19 @@ public:
         gridLayout_2->setContentsMargins(11, 11, 11, 11);
         gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
         gridLayout_2->setHorizontalSpacing(15);
-        label = new QLabel(centralWidget);
-        label->setObjectName(QStringLiteral("label"));
-        QSizePolicy sizePolicy1(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
-        sizePolicy1.setHorizontalStretch(0);
-        sizePolicy1.setVerticalStretch(0);
-        sizePolicy1.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
-        label->setSizePolicy(sizePolicy1);
-        label->setMinimumSize(QSize(720, 480));
-        label->setStyleSheet(QStringLiteral("Background-color: #000;"));
-
-        gridLayout_2->addWidget(label, 3, 0, 1, 1);
-
         scrollArea = new QScrollArea(centralWidget);
         scrollArea->setObjectName(QStringLiteral("scrollArea"));
-        QSizePolicy sizePolicy2(QSizePolicy::Fixed, QSizePolicy::Fixed);
-        sizePolicy2.setHorizontalStretch(0);
-        sizePolicy2.setVerticalStretch(0);
-        sizePolicy2.setHeightForWidth(scrollArea->sizePolicy().hasHeightForWidth());
-        scrollArea->setSizePolicy(sizePolicy2);
+        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(scrollArea->sizePolicy().hasHeightForWidth());
+        scrollArea->setSizePolicy(sizePolicy1);
         scrollArea->setMinimumSize(QSize(300, 550));
         scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
         scrollArea->setWidgetResizable(true);
         scrollAreaWidgetContents_3 = new QWidget();
         scrollAreaWidgetContents_3->setObjectName(QStringLiteral("scrollAreaWidgetContents_3"));
-        scrollAreaWidgetContents_3->setGeometry(QRect(0, 0, 281, 562));
+        scrollAreaWidgetContents_3->setGeometry(QRect(0, 0, 281, 621));
         verticalLayout_3 = new QVBoxLayout(scrollAreaWidgetContents_3);
         verticalLayout_3->setSpacing(6);
         verticalLayout_3->setContentsMargins(11, 11, 11, 11);
@@ -131,6 +126,25 @@ public:
         verticalLayout_4->setSpacing(6);
         verticalLayout_4->setContentsMargins(11, 11, 11, 11);
         verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        groupBox_19 = new QGroupBox(groupBox_13);
+        groupBox_19->setObjectName(QStringLiteral("groupBox_19"));
+        formLayout_6 = new QFormLayout(groupBox_19);
+        formLayout_6->setSpacing(6);
+        formLayout_6->setContentsMargins(11, 11, 11, 11);
+        formLayout_6->setObjectName(QStringLiteral("formLayout_6"));
+        label_35 = new QLabel(groupBox_19);
+        label_35->setObjectName(QStringLiteral("label_35"));
+
+        formLayout_6->setWidget(0, QFormLayout::LabelRole, label_35);
+
+        comboBox_3 = new QComboBox(groupBox_19);
+        comboBox_3->setObjectName(QStringLiteral("comboBox_3"));
+
+        formLayout_6->setWidget(0, QFormLayout::FieldRole, comboBox_3);
+
+
+        verticalLayout_4->addWidget(groupBox_19);
+
         groupBox_14 = new QGroupBox(groupBox_13);
         groupBox_14->setObjectName(QStringLiteral("groupBox_14"));
         formLayout = new QFormLayout(groupBox_14);
@@ -142,12 +156,12 @@ public:
 
         formLayout->setWidget(1, QFormLayout::LabelRole, label_24);
 
-        spinBox_19 = new QSpinBox(groupBox_14);
-        spinBox_19->setObjectName(QStringLiteral("spinBox_19"));
-        spinBox_19->setMaximum(1000);
-        spinBox_19->setValue(400);
+        minArea = new QSpinBox(groupBox_14);
+        minArea->setObjectName(QStringLiteral("minArea"));
+        minArea->setMaximum(1000);
+        minArea->setValue(400);
 
-        formLayout->setWidget(1, QFormLayout::FieldRole, spinBox_19);
+        formLayout->setWidget(1, QFormLayout::FieldRole, minArea);
 
 
         verticalLayout_4->addWidget(groupBox_14);
@@ -173,26 +187,26 @@ public:
 
         formLayout_2->setWidget(5, QFormLayout::LabelRole, label_27);
 
-        spinBox_20 = new QSpinBox(groupBox_15);
-        spinBox_20->setObjectName(QStringLiteral("spinBox_20"));
-        spinBox_20->setMaximum(30);
-        spinBox_20->setValue(7);
+        closeKernelSize = new QSpinBox(groupBox_15);
+        closeKernelSize->setObjectName(QStringLiteral("closeKernelSize"));
+        closeKernelSize->setMaximum(30);
+        closeKernelSize->setValue(7);
 
-        formLayout_2->setWidget(1, QFormLayout::FieldRole, spinBox_20);
+        formLayout_2->setWidget(1, QFormLayout::FieldRole, closeKernelSize);
 
-        spinBox_21 = new QSpinBox(groupBox_15);
-        spinBox_21->setObjectName(QStringLiteral("spinBox_21"));
-        spinBox_21->setMaximum(30);
-        spinBox_21->setValue(3);
+        openKernelSize = new QSpinBox(groupBox_15);
+        openKernelSize->setObjectName(QStringLiteral("openKernelSize"));
+        openKernelSize->setMaximum(30);
+        openKernelSize->setValue(3);
 
-        formLayout_2->setWidget(3, QFormLayout::FieldRole, spinBox_21);
+        formLayout_2->setWidget(3, QFormLayout::FieldRole, openKernelSize);
 
-        spinBox_22 = new QSpinBox(groupBox_15);
-        spinBox_22->setObjectName(QStringLiteral("spinBox_22"));
-        spinBox_22->setMaximum(255);
-        spinBox_22->setValue(240);
+        thresholdValue = new QSpinBox(groupBox_15);
+        thresholdValue->setObjectName(QStringLiteral("thresholdValue"));
+        thresholdValue->setMaximum(255);
+        thresholdValue->setValue(240);
 
-        formLayout_2->setWidget(5, QFormLayout::FieldRole, spinBox_22);
+        formLayout_2->setWidget(5, QFormLayout::FieldRole, thresholdValue);
 
 
         verticalLayout_4->addWidget(groupBox_15);
@@ -218,32 +232,35 @@ public:
 
         formLayout_3->setWidget(5, QFormLayout::LabelRole, label_30);
 
-        comboBox_3 = new QComboBox(groupBox_16);
-        comboBox_3->setObjectName(QStringLiteral("comboBox_3"));
+        orientation = new QComboBox(groupBox_16);
+        orientation->setObjectName(QStringLiteral("orientation"));
 
-        formLayout_3->setWidget(0, QFormLayout::FieldRole, comboBox_3);
+        formLayout_3->setWidget(0, QFormLayout::FieldRole, orientation);
 
-        spinBox_23 = new QSpinBox(groupBox_16);
-        spinBox_23->setObjectName(QStringLiteral("spinBox_23"));
-        spinBox_23->setMaximum(100);
-        spinBox_23->setValue(80);
+        inLinePercentage = new QSpinBox(groupBox_16);
+        inLinePercentage->setObjectName(QStringLiteral("inLinePercentage"));
+        inLinePercentage->setMaximum(100);
+        inLinePercentage->setValue(80);
 
-        formLayout_3->setWidget(3, QFormLayout::FieldRole, spinBox_23);
+        formLayout_3->setWidget(3, QFormLayout::FieldRole, inLinePercentage);
 
-        spinBox_24 = new QSpinBox(groupBox_16);
-        spinBox_24->setObjectName(QStringLiteral("spinBox_24"));
-        spinBox_24->setMaximum(100);
-        spinBox_24->setValue(80);
+        outLinePercentage = new QSpinBox(groupBox_16);
+        outLinePercentage->setObjectName(QStringLiteral("outLinePercentage"));
+        outLinePercentage->setMaximum(100);
+        outLinePercentage->setValue(80);
 
-        formLayout_3->setWidget(5, QFormLayout::FieldRole, spinBox_24);
+        formLayout_3->setWidget(5, QFormLayout::FieldRole, outLinePercentage);
 
 
         verticalLayout_4->addWidget(groupBox_16);
 
         groupBox_17 = new QGroupBox(groupBox_13);
         groupBox_17->setObjectName(QStringLiteral("groupBox_17"));
-        sizePolicy1.setHeightForWidth(groupBox_17->sizePolicy().hasHeightForWidth());
-        groupBox_17->setSizePolicy(sizePolicy1);
+        QSizePolicy sizePolicy2(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(groupBox_17->sizePolicy().hasHeightForWidth());
+        groupBox_17->setSizePolicy(sizePolicy2);
         formLayout_4 = new QFormLayout(groupBox_17);
         formLayout_4->setSpacing(6);
         formLayout_4->setContentsMargins(11, 11, 11, 11);
@@ -258,19 +275,19 @@ public:
 
         formLayout_4->setWidget(3, QFormLayout::LabelRole, label_32);
 
-        spinBox_25 = new QSpinBox(groupBox_17);
-        spinBox_25->setObjectName(QStringLiteral("spinBox_25"));
-        spinBox_25->setMaximum(500);
-        spinBox_25->setValue(110);
+        maxWidth = new QSpinBox(groupBox_17);
+        maxWidth->setObjectName(QStringLiteral("maxWidth"));
+        maxWidth->setMaximum(500);
+        maxWidth->setValue(110);
 
-        formLayout_4->setWidget(1, QFormLayout::FieldRole, spinBox_25);
+        formLayout_4->setWidget(1, QFormLayout::FieldRole, maxWidth);
 
-        spinBox_26 = new QSpinBox(groupBox_17);
-        spinBox_26->setObjectName(QStringLiteral("spinBox_26"));
-        spinBox_26->setMaximum(500);
-        spinBox_26->setValue(175);
+        maxHeight = new QSpinBox(groupBox_17);
+        maxHeight->setObjectName(QStringLiteral("maxHeight"));
+        maxHeight->setMaximum(500);
+        maxHeight->setValue(175);
 
-        formLayout_4->setWidget(3, QFormLayout::FieldRole, spinBox_26);
+        formLayout_4->setWidget(3, QFormLayout::FieldRole, maxHeight);
 
 
         verticalLayout_4->addWidget(groupBox_17);
@@ -291,17 +308,17 @@ public:
 
         formLayout_5->setWidget(3, QFormLayout::LabelRole, label_34);
 
-        lineEdit_3 = new QLineEdit(groupBox_18);
-        lineEdit_3->setObjectName(QStringLiteral("lineEdit_3"));
+        ipAddress = new QLineEdit(groupBox_18);
+        ipAddress->setObjectName(QStringLiteral("ipAddress"));
 
-        formLayout_5->setWidget(1, QFormLayout::FieldRole, lineEdit_3);
+        formLayout_5->setWidget(1, QFormLayout::FieldRole, ipAddress);
 
-        spinBox_27 = new QSpinBox(groupBox_18);
-        spinBox_27->setObjectName(QStringLiteral("spinBox_27"));
-        spinBox_27->setMaximum(65536);
-        spinBox_27->setValue(8080);
+        portNumber = new QSpinBox(groupBox_18);
+        portNumber->setObjectName(QStringLiteral("portNumber"));
+        portNumber->setMaximum(65536);
+        portNumber->setValue(8080);
 
-        formLayout_5->setWidget(3, QFormLayout::FieldRole, spinBox_27);
+        formLayout_5->setWidget(3, QFormLayout::FieldRole, portNumber);
 
 
         verticalLayout_4->addWidget(groupBox_18);
@@ -313,10 +330,30 @@ public:
 
         gridLayout_2->addWidget(scrollArea, 3, 1, 1, 1);
 
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        sizePolicy2.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy2);
+        label->setMinimumSize(QSize(720, 480));
+        label->setStyleSheet(QStringLiteral("Background-color: #000;"));
+
+        gridLayout_2->addWidget(label, 3, 0, 1, 1);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        horizontalLayout->addWidget(pushButton);
+
         loadVideo = new QPushButton(centralWidget);
         loadVideo->setObjectName(QStringLiteral("loadVideo"));
 
-        gridLayout_2->addWidget(loadVideo, 4, 1, 1, 1);
+        horizontalLayout->addWidget(loadVideo);
+
+
+        gridLayout_2->addLayout(horizontalLayout, 5, 1, 2, 1);
 
         PeopleCountQTClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(PeopleCountQTClass);
@@ -332,8 +369,17 @@ public:
     void retranslateUi(QMainWindow *PeopleCountQTClass)
     {
         PeopleCountQTClass->setWindowTitle(QApplication::translate("PeopleCountQTClass", "PeopleCountQT", Q_NULLPTR));
-        label->setText(QString());
         groupBox_13->setTitle(QApplication::translate("PeopleCountQTClass", "Calibration", Q_NULLPTR));
+        groupBox_19->setTitle(QApplication::translate("PeopleCountQTClass", "Show Image", Q_NULLPTR));
+        label_35->setText(QApplication::translate("PeopleCountQTClass", "Image Name", Q_NULLPTR));
+        comboBox_3->clear();
+        comboBox_3->insertItems(0, QStringList()
+         << QApplication::translate("PeopleCountQTClass", "Output", Q_NULLPTR)
+         << QApplication::translate("PeopleCountQTClass", "Threshold", Q_NULLPTR)
+         << QApplication::translate("PeopleCountQTClass", "Background Subtraction", Q_NULLPTR)
+         << QApplication::translate("PeopleCountQTClass", "Morph Open", Q_NULLPTR)
+         << QApplication::translate("PeopleCountQTClass", "Morph Close", Q_NULLPTR)
+        );
         groupBox_14->setTitle(QApplication::translate("PeopleCountQTClass", "Contours", Q_NULLPTR));
         label_24->setText(QApplication::translate("PeopleCountQTClass", "Minimum Area", Q_NULLPTR));
         groupBox_15->setTitle(QApplication::translate("PeopleCountQTClass", "Blob Extraction", Q_NULLPTR));
@@ -344,14 +390,21 @@ public:
         label_28->setText(QApplication::translate("PeopleCountQTClass", "Orientation", Q_NULLPTR));
         label_29->setText(QApplication::translate("PeopleCountQTClass", "In Line Placement (%)", Q_NULLPTR));
         label_30->setText(QApplication::translate("PeopleCountQTClass", "Out Line Placement (%)", Q_NULLPTR));
-        comboBox_3->setCurrentText(QString());
+        orientation->clear();
+        orientation->insertItems(0, QStringList()
+         << QApplication::translate("PeopleCountQTClass", "Vertical", Q_NULLPTR)
+         << QApplication::translate("PeopleCountQTClass", "Horizontal", Q_NULLPTR)
+        );
+        orientation->setCurrentText(QApplication::translate("PeopleCountQTClass", "Vertical", Q_NULLPTR));
         groupBox_17->setTitle(QApplication::translate("PeopleCountQTClass", "People Threshold Size", Q_NULLPTR));
         label_31->setText(QApplication::translate("PeopleCountQTClass", "Max Width (px)", Q_NULLPTR));
         label_32->setText(QApplication::translate("PeopleCountQTClass", "Max Height (px)", Q_NULLPTR));
         groupBox_18->setTitle(QApplication::translate("PeopleCountQTClass", "Server Settings", Q_NULLPTR));
         label_33->setText(QApplication::translate("PeopleCountQTClass", "IP Address", Q_NULLPTR));
         label_34->setText(QApplication::translate("PeopleCountQTClass", "Port", Q_NULLPTR));
-        lineEdit_3->setInputMask(QApplication::translate("PeopleCountQTClass", "000.000.000.000;_", Q_NULLPTR));
+        ipAddress->setInputMask(QApplication::translate("PeopleCountQTClass", "000.000.000.000;_", Q_NULLPTR));
+        label->setText(QString());
+        pushButton->setText(QApplication::translate("PeopleCountQTClass", "Save", Q_NULLPTR));
         loadVideo->setText(QApplication::translate("PeopleCountQTClass", "Load Video", Q_NULLPTR));
     } // retranslateUi
 
